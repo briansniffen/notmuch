6189ff2c0e865ccf2947d395391549958a17b7fe
Brian Sniffen <bts@evenmere.org>
2012-05-22 00:12:45 -0400

    Add ability to show individual messages, including MIME parts
    The MIME parts are unpacked into a directory 'static,' which happens to
    be what the web.py dev server looks for.  It would be nice if this were
    changeable so that static files could be served by
    Apache/lighty/whatever.
    
    The lack of escaping should be scaring me more than it is.  Must fix
    that before sharing this more broadly.
    
    multipart/alternative should show only one part, probably?  And
    text/plain should have better wrapping, maybe?
    
    And the UI is ugly.  But this is now something I could almost use to
    read my mail.

--- contrib/notmuch-web/nmweb.py
+++ contrib/notmuch-web/nmweb.py
@@ -3,10 +3,17 @@
 import web
 from notmuch import Database,Query,Message
 import urllib
+from datetime import datetime
+from mailbox import MaildirMessage
+import os
+import mimetypes
+
+cachedir = "static" # special for webpy server; changeable if using your own
 
 urls = (
   '/', 'index',
   '/search/(.*)', 'search',
+  '/show/(.*)', 'show',
   )
 
 db = Database()
@@ -34,10 +41,14 @@ class search:
       subj = t.get_subject()
       auths = t.get_authors()
       start,end = t.get_oldest_date(), t.get_newest_date()
+      if end-start < (60*60*24):
+        time = datetime.fromtimestamp(start).strftime('%Y %b %d %H:%M')
+      else:
+        start = datetime.fromtimestamp(start).strftime("%Y %b %d")
+        end = datetime.fromtimestamp(end).strftime("%Y %b %d")
+	time = "%s through %s" % (start,end)
       msgs = t.get_toplevel_messages()
-      yield '<h2>%s</h2>' % subj # FIXME escaping
-      yield '<p><i>%s</i></p>' % auths
-      yield '<p><b>%s&ndash;%s</b></p>' % (start,end)
+      yield '<h2>%s</h2><p><i>%s</i></p><p><b>%s</b></p>' % (subj,auths,time) # FIXME escaping
       yield show_msgs(msgs)
     yield '</body></html>'
 
@@ -49,11 +60,66 @@ def show_msgs(msgs):
     if flag: red='red'
     frm = msg.get_header('From')
     subj = msg.get_header('Subject')
+    lnk = urllib.quote_plus(msg.get_message_id())
     rs = show_msgs(msg.get_replies())
-    r += '<li><font color=%s>%s&mdash;%s</font> %s</li>' % (red,frm,subj,rs)
+    r += '<li><font color=%s>%s&mdash;<a href="/show/%s">%s</a></font> %s</li>' % (red,frm,lnk,subj,rs)
   r += '</ul>'
   return r
-    
+
+class show:
+  def GET(self,mid):
+    web.header('Content-type', 'text/html')
+    web.header('Transfer-Encoding', 'chunked')
+    q = Query(db,'id:'+mid)
+    m = list(q.search_messages())[0]
+    subj = m.get_header('Subject')
+    yield '<html><head><title>Brian\'s mail: %s</title></head><body>' % subj
+    headers = ['Subject', 'To', 'Cc', 'From', 'Date']
+    # FIXME parse to/cc/from with email.utils.parseaddr
+    # FIXME add reply-all link with email.urils.getaddresses
+    for header in headers:
+      yield '<p><b>%s:</b>%s</p>' % (header,m.get_header(header))
+    yield '<hr>'
+    msg = MaildirMessage(open(m.get_filename()))
+    counter = 0
+    for part in msg.walk():
+      if part.get_content_maintype() == 'multipart': continue
+      elif part.get_content_maintype() == 'text':
+        if part.get_content_subtype() == 'plain':
+          yield '<pre>'
+	  yield part.get_payload(decode=True)
+	  yield '</pre>'
+        elif part.get_content_subtype() == 'html':
+          yield part.get_payload(decode=True)
+        else:
+          filename = link_to_cached_file(part,mid,counter)
+	  counter += 1
+	  yield '<a href="%s">%s (%s)</a>' % (os.path.join('/static',mid,filename),filename,part.get_content_type())
+      elif part.get_content_maintype() == 'image':
+        filename = link_to_cached_file(part,mid,counter)
+        counter += 1
+        yield '<img src="%s" alt="%s">' % (os.path.join('/static',mid,filename),filename)
+      else:
+        filename = link_to_cached_file(part,mid,counter)
+        counter += 1
+        yield '<a href="%s">%s (%s)</a>' % (os.path.join('/static',mid,filename),filename,part.get_content_type())
+    yield '</body></html>'
+
+def link_to_cached_file(part,mid,counter):
+    filename = part.get_filename()
+    if not filename:
+      ext = mimetypes.guess_extension(part.get_content_type())
+      if not ext:
+        ext = '.bin'
+      filename = 'part-%03d%s' % (counter, ext)
+    try:
+      os.makedirs(os.path.join(cachedir,mid))
+    except OSError:
+      pass
+    fp = open(os.path.join(cachedir, mid, filename), 'wb') # FIXME escape mid,filename
+    fp.write(part.get_payload(decode=True))
+    fp.close()
+    return filename
 
 if __name__ == '__main__': 
     app = web.application(urls, globals())
